df21a44f6ea504829127b53f16a008309ca6a9ef
m3dus444 <julien.helie@efrei.net>
2021-06-22 01:46:27 +0200

    maj

--- app/testfunc.py
+++ app/testfunc.py
@@ -11,39 +11,6 @@ import win32security
 import getSessionUser
 
 
-if sys.version[0] == '2':
-    input = raw_input
-    import _winreg as wreg
-else:
-    import winreg as wreg
-
-username = getSessionUser.getuser()  # replace all os.getLogin() by username
-try:
-    try:
-        key = wreg.OpenKey(wreg.HKEY_LOCAL_MACHINE, "Software\\Policies\\Google\\Chrome", 0, wreg.KEY_ALL_ACCESS)
-    except:
-        key = wreg.CreateKey(wreg.HKEY_LOCAL_MACHINE, "Software\\Policies\\Google\\Chrome")
-
-    try:
-        wreg.QueryValueEx(key, 'DefaultDownloadDirectory')
-    except:
-        wreg.SetValueEx(key, 'DefaultDownloadDirectory', 0, wreg.REG_SZ, 'c:\\users\\' + username +
-                        '\\Downloads')
-
-    try:
-        wreg.QueryValueEx(key, 'DownloadDirectory')
-    except:
-        wreg.SetValueEx(key, 'DownloadDirectory', 0, wreg.REG_SZ, 'c:\\users\\' + username +
-                        '\\Downloads')
-
-    print(" *** KEYS SUCCESSFULLY ADDED TO REGISTRY ***")
-    print(wreg.QueryValueEx(key, 'DefaultDownloadDirectory'))
-    print(wreg.QueryValueEx(key, 'DownloadDirectory'))
-
-    key.Close()
-
-except:
-    print(r"/!\ Couldn't access or modify registry :( /!\ ")
 
 """
 
